17460cab28816a6b9fb4d664b131668ce426908f
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2019-05-17 15:01:03 +0200

    Import clean-up in AccelerometerRecord

--- src/main/scala/eu/qrowd_project/wp6/transportation_mode_learning/util/AccelerometerRecord.scala
+++ src/main/scala/eu/qrowd_project/wp6/transportation_mode_learning/util/AccelerometerRecord.scala
@@ -2,6 +2,4 @@ package eu.qrowd_project.wp6.transportation_mode_learning.util
 
 import java.sql.Timestamp
 
-import com.datastax.driver.core.Row
-
 case class AccelerometerRecord(x: Double, y: Double, z: Double, timestamp: Timestamp)
