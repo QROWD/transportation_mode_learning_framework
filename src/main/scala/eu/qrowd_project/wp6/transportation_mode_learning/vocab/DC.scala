d985a78939d0a792a5ac67aa58230e15faef2e78
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2019-09-04 09:23:44 +0200

    Added (incomplete) DC vocabulary

---
+++ src/main/scala/eu/qrowd_project/wp6/transportation_mode_learning/vocab/DC.scala
@@ -0,0 +1,21 @@
+package eu.qrowd_project.wp6.transportation_mode_learning.vocab
+
+import org.apache.jena.rdf.model.{Property, Resource, ResourceFactory}
+
+/**
+  * This vocabulary is incomplete!!!
+  *
+  * TODO: Finish this!
+  */
+object DC {
+  val ns = "http://purl.org/dc/elements/1.1/"
+
+  // Classes
+
+  // Object properties
+  val creator: Property = ResourceFactory.createProperty(ns + "creator")
+
+  // Datatype properties
+
+  // Individuals
+}
