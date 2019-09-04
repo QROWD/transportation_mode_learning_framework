0a1689662f42840a66906198b1e12a006ea63bbe
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2019-09-04 09:48:30 +0200

    Added (incomplete) MEX-Perf vocabulary

---
+++ src/main/scala/eu/qrowd_project/wp6/transportation_mode_learning/vocab/MexPerf.scala
@@ -0,0 +1,10 @@
+package eu.qrowd_project.wp6.transportation_mode_learning.vocab
+
+/**
+  * This vocabulary object is incomplete!!!
+  *
+  * TODO: Finish this!
+  */
+object MexPerf {
+  val ns = "http://mex.aksw.org/mex-perf#"
+}
